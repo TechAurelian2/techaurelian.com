4d33e38a404cdfd09836fbfe7aa7bdb273864419
TechAurelian <dev@techaurelian.com>
2022-11-16 09:50:16 +0200

    feat: Switch to a more fluid layout
    Also centralize page section definitions in the base layout.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: {
     files: ['./src/**/*.{html,njk,md}'],
   },
   theme: {
     extend: {
+      colors: {
+        accent: colors.indigo,
+      },
     },
   },
   darkMode: 'class',
